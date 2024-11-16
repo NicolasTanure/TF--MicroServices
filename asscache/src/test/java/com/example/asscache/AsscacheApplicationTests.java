34186a630e003af5da7b0cecefed4ff7920a946b
Arthur Fenilli <arthurfenillidesouza@gmail.com>
2024-11-16 13:27:56 -0600

    asscache vital structures created.

---
+++ asscache/src/test/java/com/example/asscache/AsscacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asscache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsscacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
